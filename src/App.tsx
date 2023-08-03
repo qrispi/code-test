01c52e303ba6b90d4d2ebdc596436354636363ea
Christopher Baum <christopherabaum@gmail.com>
2023-08-03 17:31:27 -0500

    Use function form of setTodos for handleChange

--- src/App.tsx
+++ src/App.tsx
@@ -20,10 +20,10 @@ function App() {
   const addTodo = useCallback((label: string) => {
     setTodos((prevTodos) => {
       const newTodo = {
-      id: uuid(),
-      label,
-      checked: false,
-      created_at: Date.now(),
+        id: uuid(),
+        label,
+        checked: false,
+        created_at: Date.now(),
       };
       const items = [...prevTodos, newTodo];
       const sorted = items.sort((a, b) => a.created_at - b.created_at);
@@ -32,33 +32,49 @@ function App() {
       return [...pending, ...done];
     });
   }, []);
-
+  
   const handleChange = useCallback((checked: boolean, id: string) => {
-    const active = todos.find(todo => todo.id === id)
-    const old = todos.filter(todo => todo.id !== id)
-    const items = [...old, {
-      ...active,
-      checked: checked,
-      completed_at: Date.now(),
-    },]
-    const sorted = items.sort((a, b) => a.created_at - b.created_at)
-    const done = sorted.filter(todo => todo.checked)
-    const pending = sorted.filter(todo => !todo.checked)
-    setTodos([...pending, ...done]);
-  }, []);
+    setTodos((prevTodos) => {
+      const updatedTodos = prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            checked: checked,
+            completed_at: checked ? Date.now() : undefined,
+          };
+        } else {
+          return todo;
+        }
+      });
+  
+      const sorted = [...updatedTodos].sort((a, b) => {
+        if (a.checked === b.checked) {
+          if (a.checked) {
+            return (a.completed_at || 0) - (b.completed_at || 0);
+          } else {
+            return (b.created_at || 0) - (a.created_at || 0);
+          }
+        } else {
+          return a.checked ? 1 : -1;
+        }
+      });
+  
+      return sorted;
+    });
+  }, []);  
 
   const deleteTodo = useCallback((id: string) => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
   }, []);
-
+   
   return (
     <Wrapper>
       <Header>Todo List</Header>
       <AddInput onAdd={addTodo} />
       <TodoList>
-        {todos.map((todo) => (
+      {todos.map((todo) => (
         <TodoItem key={todo.id} {...todo} onChange={handleChange} deleteTodo={deleteTodo} />
-        ))}
+      ))}
       </TodoList>
     </Wrapper>
   );
